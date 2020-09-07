e3a85fc191f65ba884d53a920236c4fc53b218ef
pokaa3a <pkhuang@umich.edu>
2020-09-07 11:15:20 -0700

    20200907

--- problems/322. Coin Change.cc
+++ problems/322. Coin Change.cc
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,-1);
-        dp[0] = 0;
-        sort(coins.begin(), coins.end());
-        for(int i=1; i<=amount; i++){
-            for(int k=0; k<coins.size(); k++){
-                if(coins[k]>i) break;
-                if(dp[i-coins[k]]>=0){
-                    dp[i] = dp[i]>0?min(dp[i],dp[i-coins[k]]+1):dp[i-coins[k]]+1;
+        vector<int> dp(amount + 1, INT_MAX);
+        // dp[i] == minimum coins need for amount i
+        dp[0] = 0; 
+        
+        for (int i = 0; i <= amount; i++) {
+            for (int c : coins) {
+                if (c <= i && dp[i - c] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[i - c] + 1);
                 }
             }
         }
-        return dp[amount];
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
