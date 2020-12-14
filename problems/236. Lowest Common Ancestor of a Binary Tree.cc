68d54b7508c48495ffb903cd9476a2ce512874af
pokaa3a <pkhuang@umich.edu>
2020-12-13 17:53:25 -0800

    20201213

---
+++ problems/236. Lowest Common Ancestor of a Binary Tree.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <queue>
+#include <stack>
+#include <list>
+#include <sstream>
+#include <cmath>
+#include <numeric>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+void traverse(TreeNode* root) {
+	if (!root) return;
+	cout << root->val << endl;
+	traverse(root->left);
+	traverse(root->right);
+}
+
+/* Solution */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if (!root) return root;
+        
+        if (root == p || root == q) return root;
+        
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        
+        if (left && !right) return left;
+        else if (!left && right) return right;
+        else if (left && right) return root;
+        return nullptr;
+    }
+};
+
+int main() {
+	/* Solution */
+	Solution sol;
+}
