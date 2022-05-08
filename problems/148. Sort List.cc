a2caf164c097f56d159354a90c508354135415cb
pokaa3a <pkhuang@umich.edu>
2022-05-08 01:53:39 -0700

    update gitignore

--- problems/148. Sort List.cc
+++ problems/148. Sort List.cc
@@ -1,31 +1,40 @@
 #include <iostream>
 using namespace std;
 
-struct ListNode{
+struct ListNode
+{
 	int val;
 	ListNode *next;
 	ListNode(int x) : val(x), next(NULL) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-	ListNode* sortList(ListNode *head){
-		if(!head || !head->next) return head;
+	ListNode *sortList(ListNode *head)
+	{
+		if (!head || !head->next)
+			return head;
 		ListNode *middle_node = findMideleNode(head);
 		ListNode *head2 = middle_node->next;
 		middle_node->next = NULL;
 		return merge(sortList(head), sortList(head2));
 	}
 
-	ListNode* merge(ListNode *head1, ListNode *head2){
+	ListNode *merge(ListNode *head1, ListNode *head2)
+	{
 		ListNode *cur1 = head1, *cur2 = head2;
 		ListNode *dummy = new ListNode(0);
 		ListNode *node = dummy;
-		while(cur1 && cur2){
-			if(cur1->val < cur2->val){
+		while (cur1 && cur2)
+		{
+			if (cur1->val < cur2->val)
+			{
 				node->next = cur1;
 				cur1 = cur1->next;
-			} else {
+			}
+			else
+			{
 				node->next = cur2;
 				cur2 = cur2->next;
 			}
@@ -35,10 +44,13 @@ public:
 		return dummy->next;
 	}
 
-	ListNode* findMideleNode(ListNode *head){
-		if(!head->next) return head;
+	ListNode *findMideleNode(ListNode *head)
+	{
+		if (!head->next)
+			return head;
 		ListNode *fast = head, *slow = head;
-		while(fast && fast->next && fast->next->next){
+		while (fast && fast->next && fast->next->next)
+		{
 			fast = fast->next->next;
 			slow = slow->next;
 		}
@@ -46,7 +58,8 @@ public:
 	}
 };
 
-int main() {
+int main()
+{
 	ListNode *N0 = new ListNode(-1);
 	ListNode *N1 = new ListNode(5);
 	ListNode *N2 = new ListNode(3);
@@ -62,9 +75,9 @@ int main() {
 	ListNode *head = sol.sortList(N0);
 
 	ListNode *cur = head;
-	while(cur){
+	while (cur)
+	{
 		cout << cur->val << " ";
 		cur = cur->next;
 	}
-
 }
